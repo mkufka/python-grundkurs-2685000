b0b53117e54680eca3cc9bb02c063e03399e91dc
Markus Kufka <markus@donsoft.net>
2025-05-12 10:57:37 +0200

    added own file

---
+++ selbst_gemacht.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+a = 10
+b = 5
+c = 3
+
+# Addition
+summe = a + b + c
+print("Summe:", summe)
+
+# Subtraktion
+differenz = a - b - c
+print("Differenz:", differenz)
+
+# Multiplikation
+produkt = a * b * c
+print("Produkt:", produkt)
+
+# Division
+quotient = a / b
+print("Quotient:", quotient)
+
+# Ganzzahlige Division
+ganzzahl_quotient = a // c
+print("Ganzzahliger Quotient:", ganzzahl_quotient)
+
+# Modulo (Rest einer Division)
+rest = a % c
+print("Rest:", rest)
+
+# Potenzierung
+potenz = 2 ** c
+print("2 hoch 3:", potenz)
+
+# Aufgabe:
+# Führen Sie eine Addition, Subtraktion, Multiplikation und Division
+# mit zwei von Ihnen gewählten Variablen durch und geben Sie die Ergebnisse aus.
+
+meineVariable1 = 15
+meineVariable2 = 10
+
+meine_addition = meineVariable1 + meineVariable2
+print("Addition:", meine_addition)
+
+meine_subtraktion = meineVariable1 - meineVariable2
+print("Subtraktion:", meine_subtraktion)
+
+meine_multiplikation = meineVariable1 * meineVariable2
+print("Multiplikation:", meine_multiplikation)
+
+meine_division = meineVariable1 / meineVariable2
+print("Division:", meine_division)
+
+meine_ganzzahlige_division = meineVariable1 // meineVariable2
+print("Ganzzahliger Quotient:", meine_ganzzahlige_division)
